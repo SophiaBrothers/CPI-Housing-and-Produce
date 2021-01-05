ee0d66bb8db3239cfe5a1de5c36eda3eda6c3bde
Sophia Brothers <61033321+SophiaBrothers@users.noreply.github.com>
2021-01-04 19:04:45 -0600

    Update CPI_PRODUCE_HOUSING.R

--- CPI_PRODUCE_HOUSING.R
+++ CPI_PRODUCE_HOUSING.R
@@ -28,28 +28,69 @@ CPI <- plyr::rename(CPI, c("CUSR0000SAF113"= "FT.VG",
 colnames(CPI)
 any(is.na(CPI))
 
-# CREATE A YEAR ONLY DATASET----
-# make sure the date is indeed a date
+# CONVERT DATE (YMD) TO SEPERATE COLUMNS 
+
+# first make sure the date is indeed a date
 CPI$DATE <- as.Date(CPI$DATE)
 
-# extract the year and convert to numeric format
-CPI$YEAR <- as.numeric(format(CPI$DATE, "%Y"))
+library(lubridate)
+CPI <- CPI %>% 
+  dplyr::mutate(YEAR = lubridate::year(DATE), 
+                MONTH = lubridate::month(DATE), 
+                DAY = lubridate::day(DATE))
 
-# DROP THE FULL DATE TO CREATE DATASET BY YEAR 
-CPI.YEAR <- CPI[, -c(1)]
+# DROP THE FULL DATE
+CPI.YMD <- CPI[, -c(1)]
+
+# REARRANGE COLUMNS
+CPI.YMD <- CPI.YMD[, c(3,4,5,1,2)]
+head(CPI.YMD)
+
+
+# CREATE A YEAR ONLY DATASET----
+
+# How to extract the year and convert to numeric format
+#CPI$YEAR <- as.numeric(format(CPI$DATE, "%Y"))   # can skip this step. this step creates column "YEAR"
+
+# DROP MONTH AND DAY  
+CPI.YEAR <- CPI.YMD[, -c(2,3)]
 head(CPI.YEAR)
-
-
-
+summary(CPI.YEAR)
 
 # SIDE NOTE* 
 # This code will sum a column(e.i. 'housing') by year
 # aggregate(CPI.YEAR$HOUSING, by=list(year=CPI.YEAR$YEAR), FUN=sum)
 
-# spot check for quality; yes, showing the year
-head(CPI)
 
-CPI.YEAR <- 
+# BASIC PLOTS
+hist(CPI.YEAR$FT.VG)
+hist(CPI.YEAR$HOUSING)
 
-  
-# CONVERT DATE (YMD) TO SEPERATE COLUMNS
+install.packages("GGally")
+library(GGally)
+ggpairs(CPI.YEAR[,]) 
+
+# all predictors are strongly correlated
+
+install.packages("hrbrthemes")
+library(hrbrthemes)
+library(kableExtra)
+options(knitr.table.format = "html")
+library(streamgraph)
+library(viridis)
+library(DT)
+library(plotly)
+
+# LINE CHART
+ggplot(CPI.YMD, aes(x=YEAR, y= FT.VG, group= as.factor(MONTH), color=as.factor(MONTH))) +
+  geom_line() +
+  scale_color_viridis(discrete = TRUE) +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=14)
+  ) +
+  ggtitle("CPI of FRUITS & VEGGIES") +
+  theme_ipsum()
+
+
+
